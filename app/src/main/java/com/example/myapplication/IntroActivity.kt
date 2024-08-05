330715c0fd96c8af8e83a3db138e3630b8888e49
fabiodrneles <fabiodrneles@gmail.com>
2024-08-05 16:50:06 -0300

    criando a tela de intro

---
+++ app/src/main/java/com/example/myapplication/IntroActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+
+    lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+
+    }
+}
